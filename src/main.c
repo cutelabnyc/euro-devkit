cc3dfaf35c5d9dc73ba46227d8f8d02e9b81df9f
Max Ardito <maxwellardito@gmail.com>
2020-12-14 14:51:58 -0500

    blinken lights, happy life day

--- src/main.c
+++ src/main.c
@@ -1,40 +1,82 @@
-/*
- * This is a blink example using the stm32f4-template (https://github.com/sferrini/stm32f4-template)
- */
-
+#include <stm32f4xx.h>
 #include <stm32f4xx_hal.h>
 #include <stm32f4xx_hal_rcc.h>
 #include <stm32f4xx_hal_gpio.h>
-#include <system_stm32f4xx.h>
+#include <stdio.h>
 
-UART_HandleTypeDef huart2;
+void Error_Handler(void);
+static void GPIO_Config();
 void SystemClock_Config(void);
-static void MX_USART2_UART_Init(void);
+
+void SysTick_Handler(void)
+{
+    HAL_IncTick();
+    // HAL_SYSTICK_IRQHandler();
+}
+
+GPIO_InitTypeDef GPIO_InitStruct;
 
 int main(void)
 {
-    HAL_Init();
-    //HAL_RCC_ClockConfig();
-
-    GPIO_InitTypeDef GPIO_InitStruct;
-    __GPIOC_CLK_ENABLE();
-
-    /*Configure GPIO pin : PC12 */
-    GPIO_InitStruct.Pin = GPIO_PIN_12;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_OD; // digital Input
-    GPIO_InitStruct.Pull = GPIO_PULLDOWN;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+    // HAL_Init();
+    SystemClock_Config();
+    GPIO_Config();
 
     /* Infinite loop */
     /* USER CODE BEGIN WHILE */
     while (1)
     {
-        /* USER CODE END WHILE */
+        // /* USER CODE BEGIN */
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_SET);
+        HAL_Delay(200);
 
-        /* USER CODE BEGIN 3 */
-        HAL_GPIO_TogglePin(GPIOC, GPIO_PIN_12);
-        HAL_Delay(1000);
+        HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
+        HAL_Delay(200);
     }
 
     return 0; // never returns actually
+}
+
+static void GPIO_Config()
+{
+    __HAL_RCC_GPIOC_CLK_ENABLE();
+    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_9, GPIO_PIN_RESET);
+
+    /*Configure GPIO pin : PC12 */
+    GPIO_InitStruct.Pin = GPIO_PIN_9;
+    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP; // digital Output
+    GPIO_InitStruct.Pull = GPIO_PULLUP;
+    GPIO_InitStruct.Speed = GPIO_SPEED_HIGH;
+    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
+}
+
+void SystemClock_Config()
+{
+    RCC_OscInitTypeDef RCC_OscInitStruct;
+    RCC_ClkInitTypeDef RCC_ClkInitStruct;
+    RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
+    RCC_OscInitStruct.HSIState = RCC_HSI_ON;
+    RCC_OscInitStruct.HSICalibrationValue = 16;
+    RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
+    if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+    {
+        Error_Handler();
+    }
+
+    RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
+    RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_HSI;
+    RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+    RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
+    RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
+    if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
+    {
+        Error_Handler();
+    }
+    HAL_SYSTICK_Config(HAL_RCC_GetHCLKFreq() / 1000);
+    HAL_SYSTICK_CLKSourceConfig(SYSTICK_CLKSOURCE_HCLK);
+    HAL_NVIC_SetPriority(SysTick_IRQn, 0, 0);
+}
+
+void Error_Handler(void)
+{
 }
