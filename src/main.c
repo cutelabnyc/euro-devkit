db7405cbed31493dcf49904e4d9fe38b66925c5f
Max Ardito <maxwellardito@gmail.com>
2021-04-27 13:51:54 -0400

    Removed unnecessary buffers

--- src/main.c
+++ src/main.c
@@ -45,13 +45,10 @@ volatile int txFullComplete = 0;
 
 // DMA Buffers
 uint16_t rxBuf[BUF_SAMPLES];
-uint16_t genBuf[BUF_SAMPLES];
 uint16_t txBuf[BUF_SAMPLES];
 
 float32_t srcLeft[SAMPLES / 2];
 float32_t srcRight[SAMPLES / 2];
-float32_t destLeft[SAMPLES / 2];
-float32_t destRight[SAMPLES / 2];
 float freq[NUM_OSC];
 
 uint32_t adcValue;
@@ -115,7 +112,7 @@ int main(void)
         HAL_ADC_Start(&hadc1);
         if (HAL_ADC_PollForConversion(&hadc1, 5) == HAL_OK)
         {
-            adcValue = HAL_ADC_GetValue(&hadc1) * 0.01;
+            adcValue = HAL_ADC_GetValue(&hadc1);
         }
         HAL_ADC_Stop(&hadc1);
 
@@ -180,8 +177,6 @@ void HAL_I2S_RxCpltCallback(I2S_HandleTypeDef *hi2s)
 {
     UNUSED(hi2s);
     rxFullComplete = 1;
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET);
 }
 
 void HAL_I2S_TxHalfCpltCallback(I2S_HandleTypeDef *hi2s)
@@ -196,8 +191,6 @@ void HAL_I2S_RxHalfCpltCallback(I2S_HandleTypeDef *hi2s)
 {
     UNUSED(hi2s);
     rxHalfComplete = 1;
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_SET);
-    HAL_GPIO_WritePin(GPIOE, GPIO_PIN_4, GPIO_PIN_RESET);
 }
 
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
@@ -463,9 +456,6 @@ static void MX_GPIO_Init(void)
     HAL_GPIO_WritePin(GPIOE, GPIO_PIN_3 | GPIO_PIN_4 | GPIO_PIN_5 | GPIO_PIN_6, GPIO_PIN_RESET);
 
     /*Configure GPIO pin Output Level */
-    HAL_GPIO_WritePin(GPIOC, GPIO_PIN_4, GPIO_PIN_RESET);
-
-    /*Configure GPIO pin Output Level */
     HAL_GPIO_WritePin(GPIOB, LD1_Pin | LD3_Pin | LD2_Pin, GPIO_PIN_RESET);
 
     /*Configure GPIO pin Output Level */
@@ -486,13 +476,6 @@ static void MX_GPIO_Init(void)
     GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
     GPIO_InitStruct.Pull = GPIO_NOPULL;
     HAL_GPIO_Init(USER_Btn_GPIO_Port, &GPIO_InitStruct);
-
-    /*Configure GPIO pin : PC4 */
-    GPIO_InitStruct.Pin = GPIO_PIN_4;
-    GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
-    GPIO_InitStruct.Pull = GPIO_NOPULL;
-    GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-    HAL_GPIO_Init(GPIOC, &GPIO_InitStruct);
 
     /*Configure GPIO pins : LD1_Pin LD3_Pin LD2_Pin */
     GPIO_InitStruct.Pin = LD1_Pin | LD3_Pin | LD2_Pin;
