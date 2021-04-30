cc6fdd7aa166cf5c5a84e1cc2241c88d6f640c82
Max Ardito <maxwellardito@gmail.com>
2021-04-30 14:43:02 -0400

    Groundwork for adc struct

--- src/adc/adc.h
+++ src/adc/adc.h
@@ -8,17 +8,29 @@ extern "C"
 
 #include "stm32f7xx_hal.h"
 
-    // typedef struct gpio {
-    //     float val;
-    //     // GPIO_pin...
-    // } gpio_t;
+    typedef enum component {
+        POTENTIOMETER,
+        SWITCH,
+        LED,
+        INLET
+    } component_t;
+
+    typedef struct param {
+        component_t component;
+        GPIO_InitTypeDef GPIO_InitStruct;
+        uint16_t val; // TODO: Like in the rampsmooth class, how on earth do I make this mutable
+    } param_t;
 
     typedef struct adc {
         ADC_HandleTypeDef *hadc;
-        float adcValue;
+
+        param_t freq;
+        param_t numOsc;
+
     } adc_t;
 
     void ADC_init(adc_t *self, ADC_HandleTypeDef *adcx);
+    void ADC_init_param(param_t *self, component_t component);
     void ADC_processBlock(adc_t *self);
 
 #ifdef __cplusplus
